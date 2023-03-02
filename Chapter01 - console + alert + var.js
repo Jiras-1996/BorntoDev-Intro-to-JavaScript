4b0a16212a75dcb4bc28f133ce70c46fdd100966
jay_wesker <jiras.adt@gmail.com>
2023-03-02 10:43:31 +0700

    Create file

---
+++ Chapter01 - console + alert + var.js
@@ -0,0 +1,26 @@
+// Chapter 01 : ตัวแปร console , alert และ var
+
+// 1. console.log => ใช้แสดงข้อความในรูปแบบ log
+console.log("Hello World")
+console.log("Hello Jay")
+
+// 2. alert => ใช้แสดงข้อความแบบกล่องข้อความแจ้งเตือน
+alert("Hello Jay")
+alert(10)
+
+// 3. var => ใช้ในการประกาศตัวแปร และ ตัวแปรในjsเป็นแบบ case-sensitive
+var name = "Jay"
+console.log(name)
+console.log(name)
+
+// 4. การ Update ค่าในตัวแปร
+name = "Pop"
+console.log(name)
+name = "James"
+console.log(name)
+
+// Challenge : ประกาศตัวแปร firstname กับ lastname
+var firstname = "jiras"
+var lastname = "adthsutanan"
+console.log(firstname)
+console.log(lastname)
---
+++ Chapter02 - Data Usage.js
@@ -0,0 +1,24 @@
+// Chapter 02 : การใช้งานข้อมูล (Data Usage)
+
+// 1. String
+// เครื่องหมาย "" => เอาไว้บอกคอมพิวเตอร์ว่าเป็นข้อมูลไม่ใช่ส่วนของโค้ด
+// มักใช้ในข้อมูลประเภท ข้อความ หรือ string 
+
+alert("Hello")
+var name = "Jay"
+alert(typeof name)
+
+// 2. Number
+// ข้อมูลประเภทตัวเลข (Number)
+
+alert(10)
+var age = 12
+alert(typeof age)
+alert(typeof 12)
+
+// 3. Boolean
+// ข้อมูลประเภท Boolean (True/False)
+alert(typeof true)
+alert(typeof false)
+console.log(typeof true)
+console.log(typeof false)
