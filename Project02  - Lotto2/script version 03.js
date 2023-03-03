97ab5fa6d4566b1e347c72c9800a96f0adbb6625
jay_wesker <jiras.adt@gmail.com>
2023-03-03 13:04:20 +0700

    Update file

--- Project02  - Lotto2/script version 03.js
+++ Project02  - Lotto2/script version 03.js
@@ -2,6 +2,14 @@
 
 // 1. กรอกตัวเลขเพื่อรับค่าเข้ามา
 let lotto_number = prompt("กรอกเลขที่ต้องการซื้อ :")
+let random_number = Math.floor(Math.random() * 100)
 
-// 2. ทำการสุ่มตัวเลขและแสดงผลค่าออกมา
-document.getElementById("random").innerHTML = Math.floor(Math.random() * 100)
+// 2. 
+document.getElementById("random").innerHTML = random_number
+
+if(lotto_number == random_number){
+    document.getElementById("result").innerHTML = "ยินดีด้วย!คุณถูกรางวัล"
+}
+else{
+    document.getElementById("result").innerHTML = "เสียใจด้วย!คุณไม่ถูกรางวัล"
+}
